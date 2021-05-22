51aa8de69b378587fbab68ae10cbd43b62b1696b
Viktor <a122212221a@gmail.com>
2021-05-22 22:41:48 +0300

    Added validations for register/login

--- Models/LoginModel.py
+++ Models/LoginModel.py
@@ -1,6 +1,6 @@
 from .FlaskFormBase import *
 
 class LoginModel(FlaskForm):
-    login=StringField("Login:", validators=[DataRequired(), Length(4)])
+    login=StringField("Login:", validators=[DataRequired(), Length(min=4, max=64)])
     password=PasswordField("Password:", validators=[DataRequired(), Length(8)])
     submit = SubmitField("Login!")
--- Models/RegisterModel.py
+++ Models/RegisterModel.py
@@ -2,5 +2,5 @@ from .FlaskFormBase import *
 from .LoginModel import LoginModel
 
 class RegisterModel(LoginModel):
-    username=StringField("Username:", validators=[DataRequired(), Length(4)])
+    username=StringField("Username:", validators=[DataRequired(), Length(min=4, max=64)])
     submit = SubmitField("Register!")
